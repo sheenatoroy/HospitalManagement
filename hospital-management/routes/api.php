c9111c080304f33c93da27a20f794f986bed3b3e
Monj <123821613+Monjing01@users.noreply.github.com>
2024-06-12 00:09:26 +0800

    stagging all the files

--- hospital-management/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::middleware('auth:api')->group(function () {
-    Route::resource('patients', PatientController::class);
-    Route::resource('doctors', DoctorController::class);
-    Route::resource('appointments', AppointmentController::class);
-});
